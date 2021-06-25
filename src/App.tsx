9f83110bfeb1df4669e4985b75941a3bb23d6d97
luk_96 <luk_96@mail.ru>
2021-06-25 15:41:40 +0300

    remove App old body, add user authirization check

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getUserInfo() {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if(accessToken == null)
+    return {
+      isAuthorized: false
+    };
+  
+  const response = await fetch("https://localhost:5001/User/getUserInfo", {
+    headers:{
+      "Authorization": "Bearer " + accessToken
+    }
+  })
+
+  if(response.status == 401)
+    return {
+      isAuthorized: false
+    };
+
+  const json = await response.json();
+  json.isAuthorized = true;
+  return json;
+}
+
+
+class App extends Component<any, any> {
+  constructor(props: any){
+    super(props);
+  }
+
+  async componentDidMount(){
+    const userInfo = await getUserInfo();
+    this.setState({userInfo: userInfo});
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        {
+          this.state?.userInfo != null 
+          && this.state?.userInfo.isAuthorized != true 
+          && <a href="https://oauth.yandex.ru/authorize?client_id=3b45d777976d49aea146b1d79bcd13d1&response_type=code&redirect_uri=http://localhost:3000/getToken">ввввойти через яндекс так сказать....</a>
+        }
+        {this.state?.userInfo.isAuthorized && this.state?.userInfo.email}
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
-
-
-
