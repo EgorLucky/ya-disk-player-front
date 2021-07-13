7322af886e2d0101f9fdf23b32221010b53dc43f
DESKTOP-4CDNPFM\Egor <luk_96@mail.ru>
2021-07-13 21:42:33 +0300

    update ui in FileExplorer, add fileClicked, backClicked, getFileURL functions

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -6,9 +6,26 @@ class FileExplorer extends Component<any, any> {
   }
 
   async componentDidMount(){
+    await this.getFolderContent("");
+  }
+
+  fileClicked = async (e :any) => {
+    if(e.type == 'folder')
+      await this.getFolderContent(e.path);
+    else
+      await this.getFileURL(e.path);
+  }
+
+  getFolderContent = async (path: string) => {
+
+    if(path == null)
+      path = "";
+
+    this.setState({parentFolderPath: path});
+
     const accessToken = localStorage.getItem("accessToken");
 
-    const response = await fetch("https://localhost:5001/file/get?parentFolderPath=&search=&page=1",{
+    const response = await fetch("https://localhost:5001/file/get?parentFolderPath=" + path + "&search=&page=1",{
       headers:{
         "Authorization": "Bearer " + accessToken
       }
@@ -24,24 +41,47 @@ class FileExplorer extends Component<any, any> {
       return;
     }
 
-    const json = await response.json();
+    const json = (await response.json())
+                  .map((j: any) => 
+                  {
+                    j.onClick = () => this.fileClicked(j);
+                    return j;
+                  });
     this.setState({files: json});
+  }
+
+  getFileURL = async (path: string) => {
+
+  }
+
+  backClicked = async () => {
+    await this.getFolderContent(this.state?.parentFolderPath)
   }
 
   render() {
     return (
-      <div>
-      {
-        this.state != null 
-        && this.state.files != null
-        && this.state.files.map((f: any) => {
-          return (
-            <div>
-              {f.name}
-            </div>
-          );
-        })
-      }
+      <div style={{float:"left", textAlign:"left", width:"90%"}}>
+        <button 
+          disabled=
+          { 
+            this.state?.parentFolderPath == null ||
+              this.state?.parentFolderPath == ""
+          } 
+          onClick={this.backClicked}>
+            Backkkk
+          </button>
+        
+        {
+          this.state != null 
+          && this.state?.files != null
+          && this.state?.files.map((f: any) => {
+            return (
+              <div onClick={f.onClick} style={{height:"40px", paddingTop: "15px", width:"100%", borderStyle: "solid", borderColor: "black", borderWidth: "1px"}}>
+                {f.name}
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
